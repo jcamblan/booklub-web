f4ee0a59728230e2837ce1bac7573bd93cb84c89
Julien <julien.camblan@gmail.com>
2022-01-02 00:41:10 +0100

    fix: refactor register after breaking changes

--- app/components/screen_component.rb
+++ app/components/screen_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ScreenComponent < ViewComponent::Base
-  def initialize(title:, back_path: nil, modal_toggler: false)
+  def initialize(title: nil, back_path: nil, modal_toggler: false)
     @back_path = back_path
     @title = title
     @modal_toggler = modal_toggler
